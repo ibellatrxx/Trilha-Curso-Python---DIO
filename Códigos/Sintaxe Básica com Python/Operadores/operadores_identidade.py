1dbddff5b45229cd73766f86e7ede23fe06fe641
ibellatrxx <biancasoares.jow@gmail.com>
2025-03-06 12:11:30 -0300

    finalizei ESTRUTURAS DE REPETICAO

---
+++ Códigos/Sintaxe Básica com Python/Operadores/operadores_identidade.py
@@ -0,0 +1,5 @@
+saldo = 1000
+limite = 1000
+
+print(saldo is limite) #ou seja, ocupam a mesma posição de memória
+print(saldo is not limite)
--- Códigos/Sintaxe Básica com Python/operadores_identidade.py
+++
@@ -1,5 +0,0 @@
-saldo = 1000
-limite = 1000
-
-print(saldo is limite) #ou seja, ocupam a mesma posição de memória
-print(saldo is not limite)
