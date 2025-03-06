f0bd47f34faa19670d47008da2df48e94fa58b99
Bianca Soares <125821686+ibellatrxx@users.noreply.github.com>
2025-03-05 23:55:20 -0300

    Add files via upload

---
+++ Códigos/Introdução ao Ambiente Python/conversao_tipos.py
@@ -0,0 +1,12 @@
+preco = 10.510
+idade = 19
+
+print(str(preco)) #isso é uma concatenação, uma conversão para string
+print(str(idade))
+
+texto = f"idade {idade} preco {preco}" #outro jeito de concatenar, utilizei o f antes de tudo
+#significando FLOAT e entre {} o que quiser concatenar
+print(texto)
+
+print(10 / 2) #mostra 5.0
+print(10 // 2) #mostra 5, sem ponto e 0
---
+++ Códigos/Introdução ao Ambiente Python/variaveis_constantes.py
@@ -0,0 +1,11 @@
+nome = "Bianca"
+idade = 19
+
+nome = "Brenda" #não tem problema alterar a variável no meio do código,
+#vai pegar sempre o último valor escrito
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+limiteSaqueDiario = 1000
+
