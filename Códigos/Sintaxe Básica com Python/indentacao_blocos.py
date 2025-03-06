023a34640a169797f3a3e21b07c8300b39ce04ab
ibellatrxx <biancasoares.jow@gmail.com>
2025-03-06 11:40:04 -0300

    atualizei indentacao

---
+++ Códigos/Sintaxe Básica com Python/indentacao_blocos.py
@@ -0,0 +1,14 @@
+def sacar (valor):
+    saldo = 500
+    
+    if saldo >= valor:
+        print("valor sacado!")
+        print("retire o seu dinheiro na boca do caixa.")
+    
+    print("Obrigada por ser nosso cliente!")
+        
+def depositar(valor):
+    saldo = 500
+    saldo += valor
+
+sacar (1000)
